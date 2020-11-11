8423a248065501b065daeac0bd2edae298c93201
Tomislav Zivkovic <tomislav.zivkovic@ericsson.com>
2020-11-11 16:40:53 +0100

    Spring Data JPA Query Methods

--- src/main/java/guru/springframework/recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,10 @@ package guru.springframework.recipeapp.repositories;
 import guru.springframework.recipeapp.model.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure,Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
+
 }
