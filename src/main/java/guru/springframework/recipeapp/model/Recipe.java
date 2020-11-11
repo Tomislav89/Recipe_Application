f3765bb3c99bcd61f86391e0fae97fad9b9ae2e6
Tomislav Zivkovic <tomislav.zivkovic@ericsson.com>
2020-11-11 11:47:34 +0100

    One To Many JPA Relationships

--- src/main/java/guru/springframework/recipeapp/model/Recipe.java
+++ src/main/java/guru/springframework/recipeapp/model/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.recipeapp.model;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -22,6 +23,9 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     public int getPrepTime() {
         return prepTime;
