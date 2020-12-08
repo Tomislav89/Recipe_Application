8a73224a036c6ac50f4a54abd4b7dd37ba8140eb
Tomislav Zivkovic <tomislav.zivkovic@ericsson.com>
2020-12-08 11:49:01 +0100

    Mockito Argument Capture

--- src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
@@ -1,15 +1,9 @@
 package guru.springframework.recipeapp.controllers;
 
-import guru.springframework.recipeapp.model.Category;
-import guru.springframework.recipeapp.model.UnitOfMeasure;
-import guru.springframework.recipeapp.repositories.CategoryRepository;
-import guru.springframework.recipeapp.repositories.UnitOfMeasureRepository;
 import guru.springframework.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
