5a6d69992800e451d6fa1ba967ed9b460fbe1dff
Tomislav Zivkovic <tomislav.zivkovic@ericsson.com>
2021-01-13 08:59:51 +0100

    add recipe

--- src/main/java/guru/springframework/recipeapp/controllers/RecipeController.java
+++ src/main/java/guru/springframework/recipeapp/controllers/RecipeController.java
@@ -1,10 +1,10 @@
 package guru.springframework.recipeapp.controllers;
 
+import guru.springframework.recipeapp.commands.RecipeCommand;
 import guru.springframework.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by jt on 6/19/17.
@@ -22,5 +22,21 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    //method to render the view
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    //post back
+    //@ModelAtribute to tell spring to bind the form post to RecipeCommand object
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
